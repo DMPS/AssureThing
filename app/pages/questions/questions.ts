de1eb84270ecbd57da473fe86fe14aa7226fea50
Dalton Scott <Dalton Scott>
2016-07-16 11:51:39 +0100

    changed questions to slides format

--- app/pages/questions/questions.ts
+++ app/pages/questions/questions.ts
@@ -6,14 +6,28 @@ import {Alert, NavController} from 'ionic-angular';
 })
 export class QuestionsPage {
   constructor(private nav: NavController) {
-  
   }
+  private boolean:string = 'boolean'
+  private number:string = 'number'
+  private answer:string = 'answer'
+  public answers = [] 
+  public questions = [
+    {'text':'Are there any tripping hazards?','answerType':'boolean'},
+    {'text':'Do the lights work?','answerType':'boolean'},
+    {'text':'Are there FATRTRTE any damage marks on the entrance?', 'answerType':'boolean'},
+    {'text':'How wide is the platform?','answerType':'number'},
+    {'text':'Do the oyster gates work properly?','answerType':'boolean'}
+  ]
   onSave(){
     let alert = Alert.create({
       title: 'All Done!',
       subTitle: 'Your quality assurance report has been submitted to the database.',
       buttons: ['OK']
     });
-    this.nav.present(alert);    
+    this.nav.present(alert);
+    console.log(this.answers);    
+  }
+  onclick(question){
+    console.log(question)
   }
 }
